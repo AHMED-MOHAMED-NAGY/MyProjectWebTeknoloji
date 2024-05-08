9df3ed7ec7e5e483d30640edc252be7b168139ba
ahmed mohamed <ahmedmohamed6x5@gmail.com>
2024-05-09 01:38:42 +0300

    make the navbar

---
+++ javascript.js
@@ -0,0 +1,10 @@
+// var dm = document.getElementById("darkMode");
+// image.addEventListener('click', f1());
+//  function f1(){
+//     if (dm.src = "dark.png"){
+//         dm.src = "daymode.png";
+//     }
+//     if (dm.src = "daymode.png"){
+//         dm.src = "dark.png";
+//     }
+// }
