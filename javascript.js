3d96dbeace1e97b76e812191da1986632dfe9172
ahmed mohamed <ahmedmohamed6x5@gmail.com>
2024-05-11 01:19:59 +0300

    adding CV file and 2 photos at slider at my city file

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 function darkmode(){
-    var dm = document.getElementById("darkmode");
+    //var dm = document.getElementsByTagName("darkmode");
 }
 function login(){
     window.location.href = "login.html";
