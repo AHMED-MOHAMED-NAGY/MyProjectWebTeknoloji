153cffbf6546ccba9c308f46d701099f3cf850cc
ahmed mohamed <ahmedmohamed6x5@gmail.com>
2024-05-17 23:58:58 +0300

    edit at contact form and finish the vue part

--- javascript.js
+++ javascript.js
@@ -4,3 +4,15 @@ function darkmode(){
 function login(){
     window.location.href = "login.html";
 }
+
+var vueForm = document.getElementById('vueForm');
+var jsForm = document.getElementById("jsForm");
+
+function continueWITHvue(){
+    vueForm.style.display = 'block';
+    jsForm.style.display = "none";
+}
+function continueWITHjs(){
+    jsForm.style.display = "block";
+    vueForm.style.display = 'none';
+}
